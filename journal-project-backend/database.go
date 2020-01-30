8641c289e1aa64f52b4a9725c0c38c76274268ba
Chris Paika <paika.christopher@gmail.com>
2020-01-29 20:22:00 -0500

    Attempting to persist to the database - why cant I

--- journal-project-backend/database.go
+++ journal-project-backend/database.go
@@ -1,23 +1,71 @@
 package main
 
 import (
-	"io/ioutil"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
 )
 
 type FileDatabase struct {
 }
 
-func (fileDatabase FileDatabase) saveJournalEntry(p *Entry) error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, []byte(p.Body), 0600)
+var database *sql.DB
+
+func initializeDatabase() {
+	database, _ = sql.Open("sqlite3", "./local-dev.db")
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS journal_entry (id INTEGER PRIMARY KEY, author TEXT, title TEXT, entry TEXT)")
+	something, err := statement.Exec()
+	fmt.Println("Created the schema for the table")
+	fmt.Println(err)
+	fmt.Println(something)
+	/**
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+	**/
 }
 
-func (fileDatabase FileDatabase) loadJournalEntry(title string) (*Entry, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	var stringBody = string(body)
+// TODO - add a concept of users to the application
+func (fileDatabase FileDatabase) saveJournalEntry(entry *Entry) error {
+	primaryKey := (uuid.New()).String()
+	_, err := database.Exec("INSERT INTO journal_entry VALUES($1, $2, $3, $4)", primaryKey, "root", entry.Title, entry.Body)
 	if err != nil {
-		return nil, err
+		log.Fatal("Error writing to the database")
+		log.Fatal(err)
 	}
-	return &Entry{Title: title, Body: stringBody}, nil
+	return nil
+}
+
+func (fileDatabase FileDatabase) loadJournalEntriesForAuthor(author string) (*Entry, error) {
+	//TODO understand slices
+	var results []Entry
+	var title string
+	var body string
+	rows, err := database.Query("select title, entry from journal_entry where author = root", 1)
+	if err != nil {
+		log.Fatal("Failed reading from the database")
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&title, &body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Entry found:")
+		log.Println(title, body)
+		results = append(results, Entry{Title: title, Body: body})
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil, nil
 }
