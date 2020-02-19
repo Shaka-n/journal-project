02d4b08d4ede6ce558f6865bbffd0f536720ed66
Stefan Sahagian <stefansahagian@Stefans-MBP.home>
2020-02-18 21:27:44 -0500

    Hey this is my ffirst git commit

--- journal-project-frontend/src/index.js
+++ journal-project-frontend/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import EssayForm from './EssayForm';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<EssayForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
